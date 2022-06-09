092b0b449ea2e4f24ea73805d2cbf331b00fa0ab
Alansidney da Silva(CIn) <asj2@cin.ufpe.br>
2022-06-09 14:13:19 -0300

    Começando o App de lanterna

---
+++ Decola tech 3e/dioRN/flashlight/App.js
@@ -0,0 +1,56 @@
+import React,{useState} from 'react'
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import lightOff from './assets/icons/eco-light-off.png';
+import lightOn from './assets/icons/eco-light.png';
+import logoDioOff from './assets/icons/logo-dio-white.png';
+import logoDioOn from './assets/icons/logo-dio.png';
+const App = () =>{
+  const [lightMode,setLightMode] = useState(false);
+
+  const handleChangeMode = () => setLightMode((oldLightMode)=>!oldLightMode);
+
+  return <View style={lightMode ? style.containerLight : style.container}>
+    <TouchableOpacity onPress = {handleChangeMode}>
+      
+      <Image
+        style = {lightMode ?  style.lightingOn : style.lightingOff}
+        source = {lightMode ? lightOn:lightOff}
+      />
+    </TouchableOpacity>
+
+    <Image
+      style = {lightMode ?  style.lightingOn : style.lightingOff}
+      source = {lightMode ? logoDioOn:logoDioOff}
+    />
+  </View>
+};
+
+export default App
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  containerLight:{
+    flex:1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  lightingOn:{
+    resizeMode:'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff:{
+    resizeMode:'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+    tintColor:  'white',
+  },
+});
